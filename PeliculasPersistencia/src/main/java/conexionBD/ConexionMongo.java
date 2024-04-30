5f2acc5cc15c57810fe28474761950bf969ad88b
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-04-30 13:28:42 -0700

    Se creó la clase que establece conexión con la base de datos de Mongo

---
+++ PeliculasPersistencia/src/main/java/conexionBD/ConexionMongo.java
@@ -0,0 +1,69 @@
+package conexionBD;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+
+/**
+ * Clase que gestiona la conexión a la base de datos MongoDB y proporciona métodos para abrir y cerrar la conexión, así como para obtener la colección de películas.
+ * La clase utiliza la biblioteca oficial de MongoDB para Java.
+ * Requiere una instancia de MongoDB en ejecución en localhost:27017 y una base de datos llamada "Rentas" con una colección llamada "Peliculas".
+ * 
+ * @author Asiel Apodaca Monge
+ */
+public class ConexionMongo {
+    // Cadena de conexión a la instancia de MongoDB
+    private static final String cadenaConexion = "mongodb://localhost:27017";
+    // Nombre de la base de datos
+    private static final String nombreBd = "Escuela";
+    // Nombre de la colección
+    private static final String nombreCol = "Reportes";
+    
+    // Cliente de MongoDB
+    private static MongoClient mongoClient;
+    // Base de datos MongoDB
+    private static MongoDatabase baseDeDatos;
+    // Colección MongoDB para películas
+    private static MongoCollection<Clase> coleccion;
+    
+    /**
+     * Abre una conexión a la base de datos MongoDB y obtiene la colección de películas.
+     */
+    public static void abrirConexion() {
+        
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoClientSettings configuracion = MongoClientSettings.builder().applyConnectionString(new ConnectionString(cadenaConexion)).codecRegistry(pojoCodecRegistry).build();
+        
+        mongoClient = MongoClients.create(configuracion);
+        baseDeDatos = mongoClient.getDatabase(nombreBd);
+        coleccion = baseDeDatos.getCollection(nombreCol, Clase.class);
+    }
+    
+    /**
+     * Obtiene la colección de películas. Si la conexión no está abierta, la abre antes de obtener la colección.
+     * 
+     * @return La colección de películas.
+     */
+    public static MongoCollection<Clase> obtenerColeccion() {
+        if(coleccion == null) {
+            abrirConexion();
+        }
+        return coleccion;
+    }
+    
+    /**
+     * Cierra la conexión al cliente de MongoDB.
+     */
+    public static void cerrarConexion() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
+}
